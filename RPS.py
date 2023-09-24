3918342d57f40008a30eed295ef246df70e73e1c
Duc Nguyen Minh <111528176+ducnguyen1604@users.noreply.github.com>
2023-09-24 17:58:40 +0800

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -8,7 +8,7 @@ def player(prev_play, opponent_history=[], play_order=[{}]):
   if not prev_play:
     prev_play = "S"
 
-  #skeep track the opponent history
+  #keep track the opponent history
   opponent_history.append(prev_play)
   
   #save the last five moves of opponent
@@ -22,10 +22,14 @@ def player(prev_play, opponent_history=[], play_order=[{}]):
     play_order[0][last_five] = play_order[0].get(last_five, 0) + 1
     
     last_four = "".join(opponent_history[-4:])
+    #list all potential steps
     potential_plays = [last_four + move for move in "RPS"]
-
+    #dictionary comprehension that extracts the counts of the potential next moves from the play_order dictionary, using the get method with a default value of 0 for moves that haven't occurred yet.
     sub_order = {k: play_order[0].get(k, 0) for k in potential_plays}
 
+  #make prediction
+  #If there isn't enough history for a prediction (last_five < 5), the default prediction is set to "P" (paper).
+  #Make prediction by selecting the move with the highest count from sub_order. The max function with key=sub_order.get is used to find the move with the highest count. The [-1] indexing extracts the last character of the predicted move
   prediction = "P" if len(last_five) != 5 else max(sub_order, key=sub_order.get)[-1]
 
   return {"P": "S", "R": "P", "S": "R"}[prediction]
